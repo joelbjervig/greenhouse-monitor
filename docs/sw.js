6fda02bcb8d3673e9013dcf9f27ad300a74e6ef6
joelbjervig <joelfbjervig@gmail.com>
2026-07-04 14:20:32 +0200

    Bump cache version to force update

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'greenhouse-v1';
+const CACHE_NAME = 'greenhouse-v2';
 const ASSETS = ['/', '/index.html', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
